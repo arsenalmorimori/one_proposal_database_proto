83b660694737372ecf269c9b12fdd7862df1af1e
arsenalmorimori <arsenalbautista1@gmail.com>
2025-04-17 11:53:55 +0800

    CONNECTED (club --clean)REP 2

---
+++ one_db_prototype_r2/one_db_prototype_r2/Model/Club.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace one_db_prototype_r2.Model {
+    [Table("club_main_tbl")]
+    public class Club {
+        [Key]
+        [Column("club_id")]
+        public int club_id { get; set; }
+        [Column("club_name")]
+
+        public string club_name { get; set; }
+    }
+}
---
+++ one_db_prototype_r2/one_db_prototype_r2/Program.cs
@@ -0,0 +1,50 @@
+﻿using one_db_prototype_r2.Components;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using one_db_prototype_r2.Data;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContextFactory<ClubDBContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(8, 0, 34))));
+
+builder.Services.AddDbContext<ClubDBContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(8, 0, 34))));
+builder.Services.AddQuickGridEntityFrameworkAdapter();
+
+
+builder.Services.AddQuickGridEntityFrameworkAdapter();
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+    app.UseMigrationsEndPoint();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
